3258ad7b5d536337746a5ed7f1da9f573399e082
Karan Juneja <karan.juneja1106@gmail.com>
2016-12-16 00:27:22 +0530

    Recycler View added for MainActivity

--- app/src/main/java/org/acm/nitsurat/acmsvnit/MainActivity.java
+++ app/src/main/java/org/acm/nitsurat/acmsvnit/MainActivity.java
@@ -9,14 +9,23 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private RecyclerView mCrimeRecyclerView;
+    private	EventAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +43,9 @@ public class MainActivity extends AppCompatActivity
             TextView statusTV=(TextView)findViewById(R.id.status);
             statusTV.setText("No Internet Connectivity\n\nTry Again Later");
         }
+        mCrimeRecyclerView	=	(RecyclerView)findViewById(R.id.event_list);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        updateUI();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         reset();
         navigationView.setNavigationItemSelectedListener(this);
@@ -108,5 +120,48 @@ public class MainActivity extends AppCompatActivity
     void reset(){
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.getMenu().getItem(0).setChecked(true);
+    }
+    private	class	EventHolder	extends	RecyclerView.ViewHolder	{
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private TextView mTimingTextView;
+        public	EventHolder(View itemView)	{
+            super(itemView);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.date);
+            mTimingTextView = (TextView) itemView.findViewById(R.id.time);
+        }
+    }
+    private	class EventAdapter extends RecyclerView.Adapter<EventHolder>	{
+        private List<Event> mEvents;
+        public	EventAdapter(List<Event> events)	{
+            mEvents = events;
+        }
+        @Override
+        public	EventHolder	onCreateViewHolder(ViewGroup parent, int	viewType)	{
+            LayoutInflater layoutInflater	=	LayoutInflater.from(getApplicationContext());
+            View view = layoutInflater.inflate(R.layout.list_item_event, parent, false);
+            return	new	EventHolder(view);
+        }
+        @Override
+        public	void	onBindViewHolder(EventHolder holder, int position)	{
+            Event event	=	mEvents.get(position);
+            holder.mTitleTextView.setText(event.getTitle());
+            holder.mDateTextView.setText(event.getDate());
+            holder.mTimingTextView.setText(event.getTimings());
+        }
+        @Override
+        public	int	getItemCount()	{
+            return	mEvents.size();
+        }
+    }
+    private	void	updateUI()	{
+        List<Event> events=new ArrayList<Event>();
+        for(int i=0; i < 25; i++)
+        {
+          events.add(new Event("Event "+i,"January 10, 2017","12:00 AM"));
+        }
+        mAdapter	=	new	EventAdapter(events);
+        mCrimeRecyclerView.setAdapter(mAdapter);
     }
 }
